ea7e9e09c833f43ae4e1e30bec09e70b67cbe71b
amir71 <sa.shojaei92@gmail.com>
2018-03-14 11:14:43 +0330

    Lesson 63 completed

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,29 +8,66 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+/*
+ |--------------------------------------------------------
+ |                Thread Controller Routes
+ |--------------------------------------------------------
+ */
 Route::get('threads', 'ThreadController@index');
+Route::post('threads', 'ThreadController@store');
+Route::get('threads/{channel}', 'ThreadController@index');
 Route::get('threads/create', 'ThreadController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::delete('threads/{channel}/{thread}', 'ThreadController@destroy');
-Route::post('threads', 'ThreadController@store');
-Route::get('threads/{channel}', 'ThreadController@index');
-Route::get('/threads/{channel}/{thread}/replies', 'RepliesController@index');
-
-Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
-
-Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
-Route::delete('/replies/{reply}/favorites', 'FavoritesController@destroy');
+/*
+ |--------------------------------------------------------
+ |                Replies Controller Routes
+ |--------------------------------------------------------
+ */
 Route::delete('/replies/{reply}', 'RepliesController@destroy');
 Route::patch('/replies/{reply}', 'RepliesController@update');
-
+Route::get('/threads/{channel}/{thread}/replies', 'RepliesController@index');
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
+/*
+ |--------------------------------------------------------
+ |                Favorites Controller Routes
+ |--------------------------------------------------------
+ */
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+Route::delete('/replies/{reply}/favorites', 'FavoritesController@destroy');
+/*
+ |--------------------------------------------------------
+ |                Profiles Controller Routes
+ |--------------------------------------------------------
+ */
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
+/*
+ |--------------------------------------------------------
+ |           UserNotifications Controller Routes
+ |--------------------------------------------------------
+ */
 Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotificationsController@destroy');
 Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index');
-
+/*
+ |--------------------------------------------------------
+ |          ThreadSubscriptions Controller Routes
+ |--------------------------------------------------------
+ */
 Route::post('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@store')->middleware('auth');
 Route::delete('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@destroy')->middleware('auth');
-
+/*
+ |--------------------------------------------------------
+ |            UsersController Controller Routes
+ |--------------------------------------------------------
+ */
 Route::get('api/users', 'Api\UsersController@index');
+/*
+ |--------------------------------------------------------
+ |         UsersAvatarController Controller Routes
+ |--------------------------------------------------------
+ */
+Route::post('api/users/{user}/avatar', 'Api\UsersAvatarController@store');
+
 
 Route::get('test', function () {
 
