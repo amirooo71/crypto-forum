fd7961cee588717f0b86107162ea3dc50e1667a7
amir71 <sa.shojaei92@gmail.com>
2018-03-15 15:21:07 -0400

    Lesson 74 completed :)

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class RegisterConfirmationController extends Controller
@@ -11,14 +10,14 @@ class RegisterConfirmationController extends Controller
     public function index()
     {
 
-        try {
-            $user = User::where('confirmation_token', \request('token'))->firstOrFail();
-            $user->confirm();
-        } catch (\Exception $e) {
+        $user = User::where('confirmation_token', \request('token'))->first();
+
+        if (!$user) {
             return redirect(route('threads'))
                 ->with('flash', 'Unknown token.');
         }
 
+        $user->confirm();
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed! You may post to the form.');
     }
