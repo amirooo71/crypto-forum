d0c314905f9797d7766b4dccada84e87f124c4d1
amir71 <sa.shojaei92@gmail.com>
2018-04-14 13:16:34 +0430

    Footer created

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -25,16 +25,63 @@
 </section>
 <footer class="footer">
     <div class="container">
-        <div class="content has-text-centered">
-            <p>
-                <strong>Coded</strong> by <a href="#">Boorak</a>
-                <a href="#">MIT</a>.
-            </p>
-            <p>
-                <a class="icon" href="#">
-                    <i class="fa fa-github"></i>
-                </a>
-            </p>
+        <div class="columns">
+            <div class="column is-5">
+                <h2 class="mb-3">پودونک</h2>
+                <p class="has-text-justified mb-3">
+                    امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز
+                    شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار
+                    گیرد
+                </p>
+                <ul class="is-inline-flex footer-social">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-telegram"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="column is-3 is-offset-1">
+                <div class="content">
+                    <h5 class="heading is-5 mb-3 pr-2">وبلاگ</h5>
+                    <ul class="footer-links-list">
+                        <li>هیچ مطلبی یافت نشد.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column is-3">
+                <div class="content">
+                    <h5 class="heading is-5 mb-3 pr-2">تماس با ما</h5>
+                    <ul class="footer-links-list">
+                        <li>info@podonak.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <p>میزبانی پر قدرت سرور توسط <a href="http://www.otana.com/">Otana</a></p>
+            </div>
+            <div class="column is-narrow">
+                <p>
+                    Made with <i class="fas fa-heart has-text-danger"></i> by <a href="#">Boorak</a>
+                </p>
+            </div>
         </div>
     </div>
 </footer>
