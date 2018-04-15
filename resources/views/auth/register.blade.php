5f3a138010105cff0d7c16fc2b97a3c59e300d11
amir71 <sa.shojaei92@gmail.com>
2018-04-15 15:23:12 +0430

    Register form refactored

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="hero is-fullheight">
+    <div class="hero is-large">
         <div class="hero-body">
             <div class="container">
                 <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">فرم ثبت نام</div>
-
-                        <div class="panel-body">
-                            <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-header-title">
+                                فرم ثبت نام
+                            </div>
+                        </div>
+                        <div class="card-content">
+                            <form method="POST" action="{{ route('register') }}">
                                 {{ csrf_field() }}
-
-                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label for="name" class="col-md-4 control-label">نام کاربری</label>
-
-                                    <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control" name="name"
-                                               value="{{ old('name') }}" required autofocus>
-
-                                        @if ($errors->has('name'))
-                                            <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                        @endif
+                                <div class="field is-horizontal">
+                                    <div class="field-label is-normal">
+                                        <label class="label">نام کاربری</label>
+                                    </div>
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <div class="control">
+                                                <input class="input {{ $errors->has('name') ? ' is-danger' : '' }}"
+                                                       type="text" value="{{ old('name') }}"
+                                                       name="name"
+                                                       id="name">
+                                            </div>
+                                            @if ($errors->has('name'))
+                                                <p class="help is-danger">{{ $errors->first('name') }}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <label for="email" class="col-md-4 control-label">ایمیل</label>
-
-                                    <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control" name="email"
-                                               value="{{ old('email') }}" required>
-
-                                        @if ($errors->has('email'))
-                                            <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                        @endif
+                                <div class="field is-horizontal">
+                                    <div class="field-label is-normal">
+                                        <label class="label">ایمیل</label>
+                                    </div>
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <div class="control">
+                                                <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}"
+                                                       type="email" value="{{ old('email') }}"
+                                                       name="email"
+                                                       id="email">
+                                            </div>
+                                            @if ($errors->has('email'))
+                                                <p class="help is-danger">{{ $errors->first('email') }}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                    <label for="password" class="col-md-4 control-label">رمز عبور</label>
-
-                                    <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control" name="password"
-                                               required>
-
-                                        @if ($errors->has('password'))
-                                            <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                        @endif
+                                <div class="field is-horizontal">
+                                    <div class="field-label is-normal">
+                                        <label class="label">رمز عبور</label>
+                                    </div>
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <div class="control">
+                                                <input class="input {{ $errors->has('password') ? ' is-danger' : '' }}"
+                                                       type="password"
+                                                       name="password"
+                                                       id="password">
+                                            </div>
+                                            @if ($errors->has('password'))
+                                                <p class="help is-danger">{{ $errors->first('password') }}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="password-confirm" class="col-md-4 control-label">تکرار رمز عبور</label>
-
-                                    <div class="col-md-6">
-                                        <input id="password-confirm" type="password" class="form-control"
-                                               name="password_confirmation" required>
+                                <div class="field is-horizontal">
+                                    <div class="field-label is-normal">
+                                        <label class="label">تکرار رمز عبور</label>
+                                    </div>
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <div class="control">
+                                                <input class="input"
+                                                       type="password"
+                                                       name="password_confirmation"
+                                                       id="password-confirm">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            ثبت نام
-                                        </button>
+                                <div class="field is-horizontal">
+                                    <div class="field-label">
+                                        <!-- Left empty for spacing -->
+                                    </div>
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <div class="control is-pulled-right">
+                                                <button type="submit" class="button is-primary">
+                                                    ثبت نام
+                                                </button>
+                                            </div>
+                                            <div class="is-clearfix"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
